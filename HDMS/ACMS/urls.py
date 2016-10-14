8a1dd4a97809aaad89a3774f423ecb001bea1e02
koyexes <koyagabriel@gmail.com>
2016-10-14 17:38:51 +0100

    Homepage link okay

--- HDMS/ACMS/urls.py
+++ HDMS/ACMS/urls.py
@@ -5,11 +5,11 @@ app_name = "ACMS"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.index, name='login_index'),
-    url(r'^homepage/$', views.homepage, name='homepage'),
-    url(r'^workpage/$', views.workpage, name='workpage'),
-    url(r'^workpage/patient/$', views.patient, name = 'patient'),
-    url(r'^workpage/drug/$', views.drug, name = 'drug'),
-    url(r'^workpage/hmo/$', views.hmo, name = 'hmo'),
+    url(r'^acms/homepage/$', views.homepage, name='homepage'),
+    url(r'^acms/workpage/$', views.workpage, name='workpage'),
+    url(r'^acms/workpage/patient/$', views.patient, name = 'patient'),
+    url(r'^acms/workpage/drug/$', views.drug, name = 'drug'),
+    url(r'^acms/workpage/hmo/$', views.hmo, name = 'hmo'),
 
 
 
